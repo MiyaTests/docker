3cc098b00072184e2f2e626c58ab7b26774048be
Lucas Kyoshi Miyazaki <lucaskmiyazaki@hotmail.com>
2020-05-12 19:15:36 -0300

    adicionei o banco de dados de usuario, ainda preciso integrar com o login

--- python-app/app.py
+++ python-app/app.py
@@ -1,9 +1,22 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
 from config import Config
 from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 @app.route("/home")
 @app.route("/")
